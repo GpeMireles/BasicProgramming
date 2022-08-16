bc417ed024f3599f6950695981b7fb7c1bafc467
Jose Guadalupe Mireles <guadalupe__mireles@hotmail.com>
2022-08-16 17:06:42 -0500

    Listo hasta punto 3.2

---
+++ ErrorException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming {
+    public class ErrorException : Exception {
+        public ErrorException(string message) : base(message) { }
+    }
+    public static class ThrowFail {
+        public static void Fail() {
+            throw new ErrorException("Error!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,14 +5,29 @@ using System.Globalization;
 namespace BasicProgramming {
     internal class Program {
         static void Main(string[] args) {
-            //string InputFile = @"C:\Users\HP\source\repos\BasicProgramming\InputFile.txt";
-            string InputFile = @"C:\Users\HP\source\repos\BasicProgramming\Auxiliar.txt";
+            /*
+            string InputFile = @"C:\Users\HP\source\repos\BasicProgramming\InputFile.txt";
             string PathFile = "C:\\Users\\HP\\source\\repos\\BasicProgramming";
             DateTime date = DateTime.Now;
             string OutputFile = string.Format($"{PathFile}\\Countries {date.ToString("yyyy-MMM-dd", CultureInfo.GetCultureInfo("en-US"))}.txt");
             GreetCountries(InputFile, OutputFile);
             Console.WriteLine(OutputFile);
-    }
+            */
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /*
+            int num1 = 10, num2 = 5;
+            DerivateClass derivada = new DerivateClass(num1, num2);
+            derivada.Suma();
+            derivada.Resta();
+            */
+            try {
+                ThrowFail.Fail();
+            }
+            catch(ErrorException e) {
+                Console.WriteLine(e.Message);
+            }
+
+        }
 
         static private void GreetCountries(string inputFile, string outputFile) {
             using(StreamWriter writer = new StreamWriter(outputFile))
