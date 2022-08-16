2247c199d7f283b935fa4139d5720ca523e99d71
Jose Guadalupe Mireles <guadalupe__mireles@hotmail.com>
2022-08-16 18:53:20 -0500

    Falta documentación

--- AbstractClass.cs
+++ AbstractClass.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace BasicProgramming {
     internal abstract class AbstractClass {
         protected int Attribute1;
-        protected int Attribute2 { get; set; }
+        protected int Attribute2;
 
         public abstract void Suma();
         public abstract void Resta();
--- Program.cs
+++ Program.cs
@@ -5,21 +5,19 @@ using System.Globalization;
 namespace BasicProgramming {
     internal class Program {
         static void Main(string[] args) {
-            /*
+            
             string InputFile = @"C:\Users\HP\source\repos\BasicProgramming\InputFile.txt";
             string PathFile = "C:\\Users\\HP\\source\\repos\\BasicProgramming";
             DateTime date = DateTime.Now;
             string OutputFile = string.Format($"{PathFile}\\Countries {date.ToString("yyyy-MMM-dd", CultureInfo.GetCultureInfo("en-US"))}.txt");
             GreetCountries(InputFile, OutputFile);
-            Console.WriteLine(OutputFile);
-            */
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////
-            /*
+            Console.WriteLine($"File Created, or Updated, path -> {OutputFile}");
+            
             int num1 = 10, num2 = 5;
             DerivateClass derivada = new DerivateClass(num1, num2);
             derivada.Suma();
             derivada.Resta();
-            */
+
             try {
                 ThrowFail.Fail();
             }
@@ -30,13 +28,23 @@ namespace BasicProgramming {
         }
 
         static private void GreetCountries(string inputFile, string outputFile) {
-            using(StreamWriter writer = new StreamWriter(outputFile))
-            using(StreamReader reader = new StreamReader(inputFile)) {
-                string line;
-                while((line = reader.ReadLine()) != null) {
-                    writer.WriteLine($"Saldos hasta {line}!");
+            try {
+                using(StreamWriter writer = new StreamWriter(outputFile))
+                using(StreamReader reader = new StreamReader(inputFile)) {
+                    string line;
+                    while((line = reader.ReadLine()) != null) {
+                        writer.WriteLine($"Saldos hasta {line}!");
+                        // int x = Int32.Parse(line);
+                    }
                 }
             }
+            catch(FileNotFoundException e) {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
+        
     }
 }
