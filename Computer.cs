c3414140d42ef3a35d73d4e3960c7e283a576b19
Jose Guadalupe Mireles <guadalupe__mireles@hotmail.com>
2022-08-17 09:30:30 -0500

    Abstraction inplemented

---
+++ Computer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming {
+    internal abstract class Computer {
+        protected bool ConnectedToInternet;
+        protected bool Power;
+
+        public abstract void Connect();
+        public abstract void OnOff();
+
+    }
+}
---
+++ Laptop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming {
+    internal sealed class Laptop : Computer {
+        public Laptop(bool connected, bool power) {
+            ConnectedToInternet = connected;
+            Power = power;
+        }
+
+        public override void Connect() {
+            if(Power) {
+                ConnectedToInternet = true;
+                Console.WriteLine("You have acces to internet via Wifi");
+            }
+            else {
+                ConnectedToInternet = false;
+                Console.WriteLine("The device is Power Off");
+            }
+        }
+
+        public override void OnOff() {
+            Power = !Power;
+        }
+    }
+}
